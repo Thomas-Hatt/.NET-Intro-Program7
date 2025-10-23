f74544649246700417f0c576962cc2fb9e691ceb
Thomas-Hatt <thatt1@mcc.edu>
2025-10-23 10:52:12 -0400

    Program upload

---
+++ frmMovies.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Program_7
+{
+    partial class frmMovies
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMovies));
+            this.cboMovies = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.grpMovies = new System.Windows.Forms.GroupBox();
+            this.txtActors = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.cboGenre = new System.Windows.Forms.ComboBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.grpMovies.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboMovies
+            // 
+            this.cboMovies.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMovies.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboMovies.FormattingEnabled = true;
+            this.cboMovies.Location = new System.Drawing.Point(112, 322);
+            this.cboMovies.Name = "cboMovies";
+            this.cboMovies.Size = new System.Drawing.Size(255, 38);
+            this.cboMovies.TabIndex = 0;
+            this.cboMovies.SelectedIndexChanged += new System.EventHandler(this.cboMovies_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(539, 319);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(127, 41);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(691, 319);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(127, 41);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(843, 319);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(127, 41);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // grpMovies
+            // 
+            this.grpMovies.Controls.Add(this.label4);
+            this.grpMovies.Controls.Add(this.label3);
+            this.grpMovies.Controls.Add(this.label2);
+            this.grpMovies.Controls.Add(this.label1);
+            this.grpMovies.Controls.Add(this.txtActors);
+            this.grpMovies.Controls.Add(this.btnCancel);
+            this.grpMovies.Controls.Add(this.txtDescription);
+            this.grpMovies.Controls.Add(this.btnUpdate);
+            this.grpMovies.Controls.Add(this.cboGenre);
+            this.grpMovies.Controls.Add(this.txtTitle);
+            this.grpMovies.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpMovies.Location = new System.Drawing.Point(112, 366);
+            this.grpMovies.Name = "grpMovies";
+            this.grpMovies.Size = new System.Drawing.Size(882, 576);
+            this.grpMovies.TabIndex = 4;
+            this.grpMovies.TabStop = false;
+            this.grpMovies.Text = "Movie Details";
+            // 
+            // txtActors
+            // 
+            this.txtActors.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtActors.Location = new System.Drawing.Point(321, 102);
+            this.txtActors.Name = "txtActors";
+            this.txtActors.Size = new System.Drawing.Size(531, 39);
+            this.txtActors.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(30, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 37);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Description: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(30, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 37);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Genre: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(30, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Main Actors: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(30, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Movie Title: ";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(721, 517);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(127, 41);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescription.Location = new System.Drawing.Point(321, 226);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(527, 273);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(579, 517);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(127, 41);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // cboGenre
+            // 
+            this.cboGenre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboGenre.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboGenre.FormattingEnabled = true;
+            this.cboGenre.Items.AddRange(new object[] {
+            "Action",
+            "Adventure",
+            "Comedy",
+            "Drama",
+            "Horror",
+            "Romance"});
+            this.cboGenre.Location = new System.Drawing.Point(321, 156);
+            this.cboGenre.Name = "cboGenre";
+            this.cboGenre.Size = new System.Drawing.Size(531, 40);
+            this.cboGenre.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(321, 48);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(531, 39);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(112, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(859, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(106, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(236, 32);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Select a Movie Title: ";
+            // 
+            // frmMovies
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1026, 973);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.grpMovies);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cboMovies);
+            this.Name = "frmMovies";
+            this.Text = "My Movie Collection";
+            this.Load += new System.EventHandler(this.frmMovies_Load);
+            this.grpMovies.ResumeLayout(false);
+            this.grpMovies.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboMovies;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox grpMovies;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.ComboBox cboGenre;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtActors;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ frmMovies.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_7
+{
+    public partial class frmMovies : Form
+    {
+        // Create list of movies
+        List<MovieInformation> myMovies = new List<MovieInformation>();
+        public frmMovies()
+        {
+            InitializeComponent();
+        }
+
+        private bool ValidateMovieFields()
+        {
+            // Create an array that stores each input field
+            var fields = new string[]
+            {
+                txtTitle.Text,
+                txtActors.Text,
+                cboGenre.Text,
+                txtDescription.Text
+            };
+
+            // Check to see if any of the fields contain a null value or are empty
+            if (fields.Any(string.IsNullOrWhiteSpace))
+            {
+                MessageBox.Show("Please enter values for all fields");
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private struct MovieInformation
+        {
+            public string movieTitle;
+            public string actors;
+            public string genre;
+            public string description;
+        }
+
+        private void DisplayMovie()
+        {
+            txtTitle.Text = myMovies[cboMovies.SelectedIndex].movieTitle;
+            txtActors.Text = myMovies[cboMovies.SelectedIndex].actors;
+            txtDescription.Text = myMovies[cboMovies.SelectedIndex].description;
+            cboGenre.Text = myMovies[cboMovies.SelectedIndex].genre;
+        }
+
+        private void EnableControls (bool enabled)
+        {
+            // Set btnAdd, btnEdit, btnDelete, cboMovies enabled property
+            // to the Boolean value passed to the method.
+            btnAdd.Enabled = enabled;
+            btnEdit.Enabled = enabled;
+            btnDelete.Enabled = enabled;
+            cboMovies.Enabled = enabled;
+
+            // Disable everything in the groupbox except for the labels
+            foreach (Control ctrl in grpMovies.Controls)
+            {
+                if (ctrl is TextBox || ctrl is ComboBox || ctrl is Button)
+                {
+                    ctrl.Enabled = !enabled;
+                }
+            }
+        }
+
+        private void LoadMovieList()
+        {
+            // Clear the movie list
+            cboMovies.Items.Clear();
+
+            // Loop through the list to display each movie
+            for (int i = 0; i < myMovies.Count; i++)
+            {
+                cboMovies.Items.Add(myMovies[i].movieTitle);
+            }
+
+            // If the movie count is greater than 0, set the selected index to 0
+            if (cboMovies.Items.Count > 0)
+            {
+                cboMovies.SelectedIndex = 0;
+                
+                // Call DisplayMovie to update textboxes
+                DisplayMovie();
+            }
+        }
+
+        private void clearMovieInformation()
+        {
+            txtActors.Clear();
+            txtTitle.Clear();
+            txtDescription.Clear();
+            cboGenre.SelectedIndex = -1;
+        }
+
+        private void LoadSampleMovies()
+        {
+            // Add Movies
+            myMovies.Add(new MovieInformation
+            {
+                movieTitle = "Casablanca",
+                actors = "Humphrey Bogart, Ingrid Bergman",
+                genre = "Romance",
+                description = "\"Casablanca\" is a classic romantic drama set during World War II. " +
+                "The film revolves around Rick Blaine, " +
+                "an American expatriate who runs a nightclub in Casablanca, Morocco."
+            });
+
+            myMovies.Add(new MovieInformation
+            {
+                movieTitle = "Avengers: Endgame",
+                actors = "Robery Downey Jr., Chris Evans",
+                genre = "Action",
+                description = "Following the catastrophic events of \"Avengers: Infinity War,\" the remaining Avengers" +
+                " and their allies work to undo the devastation caused by Thanos, " +
+                "who wiped out half of all life in the universe with the snap of his fingers."
+            });
+            myMovies.Add(new MovieInformation
+            {
+                movieTitle = "Titanic (1997)",
+                actors = "Leonardo DiCaprio, Kate Winslet",
+                genre = "Romance",
+                description = "Titanic tells the story of the ill-fated RMS Titanic, " +
+                "focusing on the fictional romance between Jack Dawson (Leonardo DiCaprio) " +
+                "and Rose DeWitt Bukater (Kate Winslet)."
+            });
+            myMovies.Add(new MovieInformation
+            {
+                movieTitle = "Furious 7",
+                actors = "Vin Diesel, Dwayne Johnson",
+                genre = "Action",
+                description = "The film follows Dominic Toretto (Vin Diesel) and his crew " +
+                "as they face a new threat from Deckard Shaw (Jason Statham), " +
+                "a rogue assassin seeking revenge for the defeat of his brother. "
+            });
+        }
+
+
+        private void frmMovies_Load(object sender, EventArgs e)
+        {
+            // Call LoadSampleMovies and LoadMovieList at form load
+            LoadSampleMovies();
+            LoadMovieList();
+
+            // Disable controls for the groupbox
+            EnableControls(true);
+        }
+
+        private void cboMovies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Update textboxes when the movie selection changes
+            DisplayMovie();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Enable controls for the groupbox
+            EnableControls(false);
+
+            // Clear movie information
+            clearMovieInformation();
+
+            // Set the update button tag to "A"
+            btnUpdate.Tag = "A";
+
+            
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            // Verify that a movie was selected
+            if (cboMovies.SelectedIndex > -1)
+            {
+                // Display the movie information
+                DisplayMovie();
+
+                // Enable controls for the groupbox
+                EnableControls(false);
+
+                // Set the update button tag to "E"
+                btnUpdate.Tag = "E";
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Verify that a movie was selected
+            if (cboMovies.SelectedIndex > -1)
+            {
+                // Confirm that the user wants to delete the movie
+                string message = "Do you want to delete " + cboMovies.Text + "?";
+                string title = "Delete Movie";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show(message, title, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    {
+                        // Remove the movie from the list
+                        myMovies.RemoveAt(cboMovies.SelectedIndex);
+
+                        // Remove the movie from the ComboBox
+                        cboMovies.Items.RemoveAt(cboMovies.SelectedIndex);
+
+                        // Clear the text boxes and ComboBox
+                        clearMovieInformation();
+
+                        // Refresh the movie list
+                        LoadMovieList();
+                    }
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Disable controls for the groupbox
+            EnableControls(true);
+
+            // Display movie information
+            DisplayMovie();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Check to see if there are any empty fields
+            if (!ValidateMovieFields()) return;
+
+            // Create a temporary structure to gather data
+            MovieInformation tempMovie = new MovieInformation();
+            tempMovie.movieTitle = txtTitle.Text;
+            tempMovie.actors = txtActors.Text;
+            tempMovie.genre = cboGenre.Text;
+            tempMovie.description = txtDescription.Text;
+
+            // Remember the currently selected index
+            int selectedIndex = cboMovies.SelectedIndex;
+
+            // If the update button tag is set to "A", add the movie to the myMovies list
+            // Otherwise, overwrite the movie in the myMovies list based on the current selected index
+            if (btnUpdate.Tag.ToString() == "A")
+            {
+                myMovies.Add(tempMovie);
+                selectedIndex = myMovies.Count - 1;
+            }
+            else
+            {
+                myMovies[cboMovies.SelectedIndex] = tempMovie;
+            }
+
+            // Refresh the movie list
+            LoadMovieList();
+
+            // Set the ComboBox back to the previously selected index
+            cboMovies.SelectedIndex = selectedIndex;
+
+            // Disable controls for the groupbox
+            EnableControls(true);
+        }
+    }
+}
